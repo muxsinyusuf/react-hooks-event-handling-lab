8224db01ad9482a8fd22ba59740d29e406b6fe04
muxsinyusuf <mucsin.yusuf@student.moringaschool.com>
2023-08-19 15:48:31 +0300

    Lab Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+function EyesonMe() {
+  const onFocus = () => {
+    console.log("good");
+  };
+  const onBlur = () => {
+    console.log("Hey Eyes on me");
+  };
+  return (
+    <div>
+      <button onFocus={onFocus} onBlur={onBlur}>
+        EyesOnMe
+      </button>
+    </div>
+  );
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,14 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const inputHandler = () => {
+    console.log("Entering password");
+  };
+  return (
+    <div>
+      <input type="password" onChange={inputHandler}></input>
+    </div>
+  );
 }
 
 export default Keypad;
